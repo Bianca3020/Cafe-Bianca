9e2dd469c9bde4e47737f43478db2bbc45676386
Bianca <biancaputri3020@gmail.com>
2026-05-28 19:58:50 +0900

    feat: define product category model

--- app/src/main/java/com/biancaputri/pos/model/ModelKategori.kt
+++ app/src/main/java/com/biancaputri/pos/model/ModelKategori.kt
@@ -1,44 +1,11 @@
 package com.biancaputri.pos.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class ModelKategori(
     var idKategori: String? = null,
     var namaKategori: String? = null,
-    var statusKategori: String? = null
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(idKategori)
-        parcel.writeString(namaKategori)
-        parcel.writeString(statusKategori)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int){
-        parcel.writeString(idKategori)
-        parcel.writeString(namaKategori)
-        parcel.writeString(statusKategori)
-
-    }
-
-    companion object CREATOR : Parcelable.Creator<ModelKategori> {
-        override fun createFromParcel(parcel: Parcel): ModelKategori {
-            return ModelKategori(parcel)
-        }
-
-        override fun newArray(size: Int): Array<ModelKategori?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    var statusKategori: String? = null,
+) : Parcelable

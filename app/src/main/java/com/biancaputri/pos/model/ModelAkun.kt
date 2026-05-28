e388f5d5cf0a98eb487d3b47cad744c7833fad14
Bianca <biancaputri3020@gmail.com>
2026-05-28 19:59:02 +0900

    feat: define user account and store identity model

---
+++ app/src/main/java/com/biancaputri/pos/model/ModelAkun.kt
@@ -0,0 +1,10 @@
+package com.biancaputri.pos.model
+
+data class ModelAkun(
+    val idAkun: String? = null,
+    val nama: String? = null,
+    val nomor: String? = null,
+    val email: String? = null,
+    val github: String? = null,
+    val password: String? = null
+)
